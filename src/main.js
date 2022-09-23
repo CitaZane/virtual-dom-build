a108d7dac0e82ba4ee79c78b5c889a2e9b77ae5a
CitaZane <zanekrumina2@gmail.com>
2022-09-23 11:26:15 +0300

    update createElement from object to null as a base

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-const vApp = {
-    tag: 'div',
-    attrs:{
-        id: 'app'
-    }
-};
+import createElement from './vdom/createElement'
+const vImg = createElement('img', {attrs:{src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif'}})
+const vApp = createElement('div', {attrs:{id:'app'}, children:[vImg]});
 
 console.log(vApp);
---
+++ src/vdom/createElement.js
@@ -0,0 +1,16 @@
+/* -------------------------------------------------------------------------- */
+/*                               create element                               */
+/* -------------------------------------------------------------------------- */
+// Function simply returns vDOM element
+// To allow creating elements without any options, default values provided
+
+export default (tag, { attrs = {}, children = [] } = {}) => {
+    // element first defined as null to NOT inherit js Object properties
+    // and because of that it is 'purer' - without uneccasary inheritance
+    Object.assign(vElem, {
+        tag,
+        attrs,
+        children
+    })
+    return vElem
+}
