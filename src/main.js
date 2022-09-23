497ce3a04e2881468dad5136cf63bf12a9180352
CitaZane <zanekrumina2@gmail.com>
2022-09-23 11:54:29 +0300

    add mounting funcionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import createElement from './vdom/createElement'
+import render from './vdom/render'
+import mount from './vdom/mount'
+
 const vImg = createElement('img', {attrs:{src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif'}})
 const vApp = createElement('div', {attrs:{id:'app'}, children:['Hello world',vImg]});
 
-console.log(vApp);
+const $app = render(vApp);
+mount($app, document.getElementById('app'));
